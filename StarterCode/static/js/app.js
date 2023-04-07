0af35a47e606b550b04c7f39c070d8a59f4b6927
spmims <stephenpmims@gmail.com>
2023-04-06 22:31:45 -0500

    Got all the code I think I need but not able to display demographic information.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -29,8 +29,154 @@ function init() {
 
     // Save value from sampleOne
     console.log(sampleOne)
-    // buildCharts(firstSample);
-    // buildMetadata(firstSample);
+
+    // Build initial plots
+    buildMetadata(sampleOne);
+    buildBarChart(sampleOne);
+    buildBubbleChart(sampleOne);
+    buildGaugeChart(SampleOne);
   });
-}
+};
+
+// Population of Metadata
+function buildMetadata(sample) {
+
+    // Use D3 to get all data
+    d3.json(url).then((data) => {
+
+        // Retrieve Metadata
+        let Metadata = data.Metadata;
+
+        // Filter
+        let values = Metadata.filter(result => result.id == sample);
+
+        // Save array after filtering
+        console.log(values)
+
+        // Get first index
+        let valueData = values[0];
+
+        // Clear Metadata
+        d3.select("#sample-metadata").html("");
+
+        // Use Object.entries to add each key value pair
+        Object.entries(valueData).forEach(([key, values]) => {
+            
+            // Save each key value as they are added
+            console.log(key, values);
+
+            d3.select("#sample-metadata").append("h5").text(`${key}: ${values}`);
+        });
+    });
+
+};
+
+// Bar Chart function
+function buildBarChart(sample) {
+
+    // Use D3 to get all data
+    d3.json(url).then((data) => {
+        
+        // Get sample size
+        let sampleInfo = data.samples;
+
+        // Filter on value
+        let values = sampleInfo.filter(result => result.id == sample);
+
+        // Get first index
+        let valueData = values[0];
+
+        // Get the otuIds, otuLables, and sampleValues
+        let otuIds = valueData.otuIds;
+        let otuLabels = valueData.otuLabels;
+        let sampleValues = valueData.sampleValues;
+
+        // Save data to console
+        console.log(otuIds, otuLabels, sampleValues);
+
+        // Display top 10
+        let yticks = otuIds.slice(0, 10).map(id => `OTU ${id}`).reverse();
+        let xticks = sampleValues.slice(0, 10).reverse();
+        let labels = otuLabels.slice(0, 10).reverse();
+
+        // Create Trace
+        let trace = {
+            x: xticks,
+            y: yticks,
+            text: labels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        // Layout set up
+        let layout = {
+            title: "Top 10 OTUs Present"
+        };
+
+        // Call Plotly
+        Plotly.newPlot("bar", [trace], layout)
+
+    });
+};
+
+//Bubble Chart section
+function buildBubbleChart(sample) {
+    d3.json(url).then((data) => {
+        
+        // Get sample size
+        let sampleInfo = data.samples;
+
+        // Filter on value
+        let values = sampleInfo.filter(result => result.id == sample);
+
+        // Get first index
+        let valueData = values[0];
+
+        // Get the otuIds, otuLables, and sampleValues
+        let otuIds = valueData.otuIds;
+        let otuLabels = valueData.otuLabels;
+        let sampleValues = valueData.sampleValues;
+
+        // Save data to console
+        console.log(otuIds, otuLabels, sampleValues);
+
+        // Create Trace2
+        let trace2 = {
+            x: otuIds,
+            y: sampleValues,
+            text: otuLabels,
+            mode: "markers",
+            marker: {
+                size: sampleValues,
+                color: otuIds,
+                colorscale: "Earth"
+            }
+        };
+
+        // Layout set up
+        let layout = {
+            title: "Bacteria Per Sample",
+            color: "closest",
+            xaxis: {title: "OTU ID"},
+        };
+
+        // Call Plotly
+        Plotly.newPlot("bubble", [trace], layout)
+
+    });
+};
+
+// Dashboard update feature
+function optionChanged(values) {
+
+    // save new value
+    console.log(values);
+
+    // Call functions
+    buildMetadata(values);
+    buildBarChart(values);
+    buildBubbleChart(values);
+    buildGaugeChart(values);
+};
+
 init();
